b517a7ab63db71601275e81aa2d7a85da739ef64
IsDimon <131960646+IsDimon@users.noreply.github.com>
2023-06-12 14:00:15 +0300

    Add files via upload

---
+++ zada4a2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 2
+Console.WriteLine(" Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+ {
+Console.WriteLine(" Большее число: " + a + ", меньшее число: " + b);
+}
+else 
+{
+Console.WriteLine($"Большее число: {b}, меньшее число: {a}");
+}
+
