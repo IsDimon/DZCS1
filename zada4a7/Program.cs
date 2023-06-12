84b3f230bcab74899b3090e2918a629b56503d5c
IsDimon <131960646+IsDimon@users.noreply.github.com>
2023-06-12 14:02:25 +0300

    Add files via upload

---
+++ zada4a7/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите трехзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(((number / 100) > 0) && ((number / 1000) < 1) )
+{
+int lastNumber = number % 10;
+Console.WriteLine( $"Последней цифра числа {number} является {lastNumber}" );
+}
+else {
+    Console.WriteLine("Это число не трехзначное ");
+}
