bad4def1a16d4781aeaed69f6f9defe39154db06
IsDimon <131960646+IsDimon@users.noreply.github.com>
2023-06-16 21:52:09 +0300

    Add files via upload

---
+++ dz3/zada4a23/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine());
+int I = 1;
+if (N<I)
+{
+   Console.WriteLine("Введено неверное значение N ") ;
+   return;
+
+}
+while(I<=N)
+{
+    Console.Write($"  {Math.Pow(I,3)}");
+    I++;
+}
